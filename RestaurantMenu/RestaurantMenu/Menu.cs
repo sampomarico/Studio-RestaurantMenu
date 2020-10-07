9e18ead3f95098e4a7bd90e7b5d723dce24b1cb8
Sam <samtpomarico@gmail.com>
2020-10-06 20:21:32 -0500

    First commit. Created Menu and MenuItem Class

---
+++ RestaurantMenu/RestaurantMenu/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class Menu
+    {
+        private List<MenuItem> menuItems;
+        private DateTime lastupdated;
+
+        public Menu(List<MenuItem> menuItems)
+        {
+            this.menuItems = new List<MenuItem>();
+        }
+    }
+}
---
+++ RestaurantMenu/RestaurantMenu/MenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class MenuItem
+    {
+        private double price;
+        private string description;
+        private string[] category = { "appetizer", "main course", "dessert" };
+        private DateTime dateAdded = new DateTime();
+        private Boolean newItem;
+
+        public MenuItem(double price, string description, string[] category, DateTime dateAdded, Boolean newItem)
+        {
+            this.price = price;
+            this.description = description;
+            this.category = category;
+            this.dateAdded = dateAdded;
+            this.newItem = newItem;
+        }
+
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string[] Category { get; set; }
+        public DateTime DateAdded { get; set; }
+        public Boolean NewItem { get; set; }
+    }
+
+}
