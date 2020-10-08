91d0dd4dbe5902046ba2625518e0c55bd7a00a2d
Sam <samtpomarico@gmail.com>
2020-10-08 16:45:01 -0500

    Added IsNewItem, MenuLastUpdated, PringSingleItem methods

--- RestaurantMenu/RestaurantMenu/Menu.cs
+++ RestaurantMenu/RestaurantMenu/Menu.cs
@@ -25,5 +25,27 @@ namespace RestaurantMenu
         {
             this.menuItems.Remove(deleteItem);
         }
+
+        public string IsNewItem(MenuItem item)
+        {
+            if (item.newItem == true)
+            {
+                return "The " + item.description + " is new to our menu!";
+            } else
+            {
+                return "The " + item.description + " is not a new item. It's one of our delicious favorites.";
+            }
+        }
+
+        public string MenuLastUpdated(Menu menu)
+        {
+            return "Our menu was last updated on: " + menu.lastUpdated;
+        }
+
+        public string PrintSingleItem(MenuItem item)
+        {
+            return "Item Name: " + item.description + "\nItem Price: " + item.price + "\nItem Course: " + item.category +
+                "\nDate Added to Menu: " + item.dateAdded.ToString("d");
+        }
     }
 }
--- RestaurantMenu/RestaurantMenu/Restaurant.cs
+++ RestaurantMenu/RestaurantMenu/Restaurant.cs
@@ -8,16 +8,18 @@ namespace RestaurantMenu
     {
         static void Main(string[] args)
         {
-            MenuItem fillet = new MenuItem("$29.99", "Steak", "dinner", DateTime.Now, true);
-            MenuItem bakedPotato = new MenuItem("$6.99", "Baked Potato", "appetizer", DateTime.Now, true);
-            MenuItem iceCream = new MenuItem("$4.99", "Ice Cream Sundea", "dessert", DateTime.Now, false);
-            List<MenuItem> items = new List<MenuItem> { fillet, bakedPotato, iceCream };
+            MenuItem fillet = new MenuItem("$29.99", "Steak", "Main Course", new DateTime(2020, 9, 25), true);
+            MenuItem roastChicken = new MenuItem("$19.99", "Roast Chicken", "Main Course", new DateTime(2016, 8, 26), false);
+            MenuItem bakedPotato = new MenuItem("$6.99", "Baked Potato", "Appetizer", new DateTime(2020, 5, 1), true);
+            MenuItem houseSalad = new MenuItem("$8.99", "House Salad", "Appetizer", new DateTime(2013, 1, 1), false);
+            MenuItem iceCream = new MenuItem("$4.99", "Ice Cream Sundae", "Dessert", new DateTime(2013, 1, 1), false);
+            MenuItem cremeBrulee = new MenuItem("$5.99", "Creme Brulee", "Dessert", new DateTime(2020, 10, 1), true);
+            List<MenuItem> items = new List<MenuItem> { fillet, bakedPotato, iceCream, roastChicken, houseSalad, cremeBrulee };
             Menu dinnerMenu = new Menu(items);
 
-            foreach (MenuItem item in dinnerMenu.menuItems)
-            {
-                Console.WriteLine(item.description + ", " + item.price + ", " + item.dateAdded + ", " + item.newItem);
-            }
+            
+
+            
 
 
            
